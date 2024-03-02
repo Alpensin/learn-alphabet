eb88d7cdcd7d555e9a92109259ae3b5fcc50ff27
Alpensin <lotrus@yandex.ru>
2024-03-02 22:08:10 +0300

    Changed exit from enter to escape

--- main.go
+++ main.go
@@ -77,7 +77,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "enter":
+		case "ctrl+c", "esc":
 			return m, tea.Quit
 		default:
 			if len(msg.Runes) != 1 {
